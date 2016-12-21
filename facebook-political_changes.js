8fc660f6c6b99137e81da804ed0141c484c7344d
Garrett Griess <github@thebluepig.com>
2016-12-21 12:58:42 -0600

    Update facebook-political_changes.js

--- facebook-political_changes.js
+++ facebook-political_changes.js
@@ -11,14 +11,11 @@ $(document).ready(function () {
   var style = $('<style>.removed_wrapper { margin: 30px 0 50px; padding: 0 12px; text-align: center; } .removed_wrapper span { padding: 4px; background-color: #e9ebee; text-shadow: 1px 1px 0 #ffffff; color: #90949c; } .removed_wrapper div { height: 2px; border-bottom: 1px solid #90949c; box-shadow: 1px 1px #ffffff; margin: -11px; }</style>')
   var words = [
     'Obama',
-    'Bernie',
-    'Hillary',
     'Clinton',
     'Trump',
     'Republican',
     'Democrat',
     'Fox News',
-    'Jill Stein',
     'Bible',
     'Sponsored',
     'People You May Know'
@@ -28,6 +25,11 @@ $(document).ready(function () {
     setInterval(function () {
       $('#rightCol').css('display', 'none');
       $('#contentArea').css('width', '100%');
+      $('._5qxm').css('background-color', '#ffffff');
+      $('._5z6o').css('width', 'auto');
+      $('._4a6n').css('color', '#000000');
+      $('._4a6n').css('font-size', '15px');
+      $('._4a6n').css('padding', '0');
       $.each(words, function (key, value) {
         $('._5pat:contains(' + value + ')').replaceWith('<div class="removed_wrapper"><span>REMOVED -' + value + '</span><div></div></div>');
       });
